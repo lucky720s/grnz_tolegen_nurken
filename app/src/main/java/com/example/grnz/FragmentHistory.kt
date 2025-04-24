10bb9fdedceb3d90b7a8d1db26f0002cf8a7ca3c
lucky720s <nurken2005abc@gmail.com>
2025-04-24 12:50:30 +0500

    fixed fragment history

--- app/src/main/java/com/example/grnz/FragmentHistory.kt
+++ app/src/main/java/com/example/grnz/FragmentHistory.kt
@@ -7,11 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 class FragmentHistory : Fragment() {
     private lateinit var historyRecyclerView: RecyclerView
     private lateinit var historyAdapter: HistoryAdapter
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_history, container, false)
         historyRecyclerView = view.findViewById(R.id.recycler_view_history)
         historyAdapter = HistoryAdapter(emptyList())
@@ -19,6 +21,7 @@ class FragmentHistory : Fragment() {
         historyRecyclerView.layoutManager = LinearLayoutManager(context)
         return view
     }
+
     override fun onResume() {
         super.onResume()
         (requireActivity() as? MainActivity)?.let {
